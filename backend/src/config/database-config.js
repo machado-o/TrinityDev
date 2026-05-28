df95213c4cb6ed4df33913a4da1c93008b384316
lorrayne-amorim <lorrayneamorimfc@hotmail.com>
2026-05-28 13:51:10 -0300

    ✨ feat: update database configuration for production and development environments; enhance CORS settings in server

--- backend/src/config/database-config.js
+++ backend/src/config/database-config.js
@@ -11,6 +11,7 @@ export const databaseConfig = {
 };
 */
 
+/*
 // Configuração do banco de dados no ambiente de desenvolvimento
 export const databaseConfig = {
   dialect: 'postgres',
@@ -24,22 +25,26 @@ export const databaseConfig = {
     underscored: true
   }
 };
+*/
 
-/*
+//ADICIONAR CONFIGURAÇÃO DO BANCO DE DADOS PARA O AMBIENTE DE PRODUÇÃO NO RENDER
 // Configuração do banco de dados no ambiente de produção
 export const databaseConfig = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'scv-backend-node-sequelize',
+  host: 'dpg-d8c70lmrnols739koiug-a.oregon-postgres.render.com',
+  username: 'sav_trinitydev_db_user',
+  password: 'OqGbMK162JIRckf6J9BesA08hNs7aF9M',
+  database: 'sav_trinitydev_db',
   define: {
     timestamps: true,
     freezeTableName: true,
     underscored: true
+  },
+  dialectOptions: {
+    ssl: true
   }
 };
-*/
+
 
 /*
 Passo a passo pgadmin
