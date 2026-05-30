1d7e57d86a49f10561ee3d6b0db8e0b417334b7a
Henrique <rikemachado999@gmail.com>
2026-05-29 23:06:46 -0300

    mudança que o render fez

--- backend/src/config/database-config.js
+++ backend/src/config/database-config.js
@@ -32,7 +32,7 @@ export const databaseConfig = {
   host:     process.env.DB_HOST || 'dpg-d8d3kvt7vvec73ftakb0-a.oregon-postgres.render.com',
   username: process.env.DB_USER || 'sav_trinitydev_db_user',
   password: process.env.DB_PASS || 'ppETReCruy5tVrHk8poCezdIbN94GLb4',
-  database: process.env.DB_NAME || 'sav_trinitydev_db',
+  database: process.env.DB_NAME || 'sav_trinitydev_db_c260',
   define: {
     timestamps: true,
     freezeTableName: true,
